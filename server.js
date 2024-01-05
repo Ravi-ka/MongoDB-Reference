6ae5bf07dc6472f0082330fa0974a89ff9fe428a
Ravisankar.Ka <ka.ravisankar@gmail.com>
2024-01-06 01:28:12 +0530

    GET APIs created

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 import express from "express";
-import { ConnectToMongoDB } from "./src/config/MongoDBConnection.js";
+import { ConnectToMongoDB, getDB } from "./src/config/MongoDBConnection.js";
+import { ObjectId } from "mongodb";
 
 const server = express();
 
-server.get("/book", (req, res) => {
-  res.json({ message: "Welcome to API" });
+server.get("/books", async (req, res) => {
+  const db = getDB().collection("books");
+  //const collection = db.collection("books");
+  const data = await db.find().toArray();
+  res.status(200).json({ response: data });
+});
+
+server.get("/books/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    if (ObjectId.isValid(id)) {
+      const db = getDB().collection("books");
+      const response = await db.findOne({ _id: new ObjectId(id) });
+      if (!response) {
+        return res.status(404).json({ status: false, msg: "Item Not Found" });
+      }
+      return res.status(200).json(response);
+    } else {
+      return res.json({ msg: "Not a valid ID" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ status: false, msg: "Internal Server Error" });
+  }
 });
 
 server.listen(2000, () => {
